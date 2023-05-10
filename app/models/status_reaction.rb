0ec7fd248599eddaa2eff56c16b2120fb1b361f0
Jeremy Kescher <jeremy@kescher.at>
2023-05-10 21:03:43 +0200

    Redo StatusReaction set_custom_emoji to be similar to AnnouncementReaction
    Co-authored-by: Plastikmensch <plastikmensch@users.noreply.github.com>

--- app/models/status_reaction.rb
+++ app/models/status_reaction.rb
@@ -27,6 +27,6 @@ class StatusReaction < ApplicationRecord
   private
 
   def set_custom_emoji
-    self.custom_emoji = CustomEmoji.find_by(shortcode: name, domain: account.domain) if name.blank?
+    self.custom_emoji = CustomEmoji.find_by(disabled: false, shortcode: name, domain: account.domain) if name.present?
   end
 end
