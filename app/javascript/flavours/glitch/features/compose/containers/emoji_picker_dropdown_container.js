ccffd831bda54565f520cd3a15baba33387fc4bb
neatchee <neatchee@gmail.com>
2023-07-24 21:55:10 -0700

    Reduce number of 'frequentlyUsedEmoji' to match the embiggened picker's line length

--- app/javascript/flavours/glitch/features/compose/containers/emoji_picker_dropdown_container.js
+++ app/javascript/flavours/glitch/features/compose/containers/emoji_picker_dropdown_container.js
@@ -7,7 +7,7 @@ import { changeSetting } from 'flavours/glitch/actions/settings';
 
 import EmojiPickerDropdown from '../components/emoji_picker_dropdown';
 
-const perLine = 8;
+const perLine = 7;
 const lines   = 2;
 
 const DEFAULTS = [
@@ -25,8 +25,6 @@ const DEFAULTS = [
   'weary',
   'sob',
   'sunglasses',
-  'heart',
-  'ok_hand',
 ];
 
 const getFrequentlyUsedEmojis = createSelector([
