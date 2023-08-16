2be8a9a23df8f43aba4de2d876f52c80a175c87f
neatchee <neatchee@gmail.com>
2023-08-15 22:35:06 -0700

    Replace missed reference to Halcyon

--- app/javascript/flavours/glitch/features/ui/components/sign_in_banner.jsx
+++ app/javascript/flavours/glitch/features/ui/components/sign_in_banner.jsx
@@ -44,7 +44,7 @@ const SignInBanner = () => {
   return (
     <div className='sign-in-banner'>
       <p><FormattedMessage id='sign_in_banner.text' defaultMessage='Login to follow profiles or hashtags, favourite, share and reply to posts. You can also interact from your account on a different server.' /></p>
-      <a href='https://elk.urusai.social/' className='button button--block'>Alternative UI (Halcyon)</a>
+      <a href='https://elk.urusai.social/' className='button button--block'>Alternative UI (Elk)</a>
       {signupButton}
       <a href='/auth/sign_in' className='button button--block button-tertiary'><FormattedMessage id='sign_in_banner.sign_in' defaultMessage='Login' /></a>
     </div>
